a56b45354bc44596fb667c04161fb8566e85fa98
yoon <nicealice99@gmail.com>
2021-10-03 00:26:20 +0900

    create: 1st_seminar/문자열

---
+++ 1st_seminar/string.js
@@ -0,0 +1,7 @@
+const str = "Just do it";
+const str = "Just don't do it"; // 특수문자 사용시 이스케이프 문자 넣어서 작성 가능(\)
+
+// `(백틱) 문자열
+const myName = "조윤서";
+const str = "안녕하세요 " + myName + "입니다";
+const backtickStr = `안녕하세요 ${myName}입니다`;
